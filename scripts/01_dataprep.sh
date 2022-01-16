fa8c38e0118aaefea5bfbb2700489e5a24640a3c
Marmzy <calvin.davey@gmail.com>
2022-01-16 20:26:23 +0900

    Calculate pairwise distances + missing values interpolation

--- scripts/01_dataprep.sh
+++ scripts/01_dataprep.sh
@@ -7,14 +7,16 @@ function usage(){
     if [ -n "$1" ]; then
         echo -e "${RED}→ $1${CLEAR}"
     fi
-    echo "Usage: $0 [-h help] [-v verbose] [-o output] [-t test] [-k kfold]"
+    echo "Usage: $0 [-h help] [-v verbose] [-o output] [-t test] [-k kfold] [-i interpol] [-p perc]"
     echo " -h, --help       Print this help and exit"
     echo " -v, --verbose    Print verbose messages"
     echo " -o, --output     Name of output directory where data will be stored"
     echo " -t, --test       Size of the test dataset"
     echo " -k, --kfold      Number of folds to split the training dataset into"
+    echo " -i, --interpol   Interpolate missing value"
+    echo " -p, --perc       Minimum percentage of barcodes to select cells"
     echo ""
-    echo "Example: $0 -o data -t 0.2 -k 5"
+    echo "Example: $0 -o data -t 0.2 -k 5 -i True -p 0.75"
     exit 1
 }
 
@@ -26,6 +28,8 @@ while [[ "$#" -gt 0 ]]; do
         -o|--output) OUTPUT="$2"; shift ;;
         -t|--test) TEST="$2"; shift ;;
         -k|--kfold) KFOLD="$2"; shift ;;
+        -i|--interpol) INTERPOL="$2"; shift ;;
+        -p|--perc) PERC="$2"; shift ;;
     esac
     shift
 done
@@ -35,6 +39,8 @@ if [ -z "$VERBOSE" ]; then VALUE_V=false; else VALUE_V=true; fi;
 if [ -z "$OUTPUT" ]; then OUT_DIR="data"; else OUT_DIR=$OUTPUT; fi;
 if [ -z "$TEST" ]; then VALUE_T=0.2; else VALUE_T=$TEST; fi;
 if [ -z "$KFOLD" ]; then usage "Number of kfolds is not specified"; else VALUE_K=$KFOLD; fi;
+if [ -z "$INTERPOL" ]; then usage "Option to interpolate missing values"; else VALUE_I=$INTERPOL; fi;
+if [ -z "$PERC" ]; then usage "Percentage of barcodes to select cells is not specified"; else VALUE_P=$PERC; fi;
 
 
 #Making the output directory and subsequent subdirectories if it doesn't exist yet
@@ -62,4 +68,6 @@ python3 ${PWD%/*}/src/data_prep.py \
         --verbose ${VALUE_V} \
         --output ${OUT_DIR} \
         --test ${VALUE_T} \
-        --kfold ${VALUE_K}
+        --kfold ${VALUE_K} \
+        --interpol ${VALUE_I} \
+        --perc ${VALUE_P}
